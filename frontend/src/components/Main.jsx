746e5362701d0e006b3be119901b1944ecb272d0
Filip <filip.sundholm@utb.ecutbildning.se>
2022-01-04 11:21:38 +0100

    lib choice with styling

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Main = ({ children }) => {
     const navigate = useNavigate();
 
+    const [chosen, setChosen] = useState(false)
+
+    useEffect(() => {
+        setChosen(false);
+        console.log('hello')
+    }, [])
+
+    const handleClick = (page) => {
+        navigate(page);
+        setChosen(!chosen);
+    }
+
     return (
         <div className="home">
-            <div className="header">
-                <h1>Pick the library of your choice</h1>
-                <div className="btnBox">
-                    <button 
-                    className="btn" 
-                    onClick={() => navigate('/books')} 
-                    >Books</button>
-
-                    <button 
-                    className="btn" 
-                    onClick={() => navigate('/movies')} 
-                    >Movies</button>
+            {!chosen && <div className="header">
+                <div onClick={() => handleClick('/movies')} className="moviesBtn hover">
+                    <h1 className="titleChoice">Movies</h1>
                 </div>
-            </div>
+                <div onClick={() => handleClick('/books')} className="booksBtn hover">
+                    <h1 className="titleChoice">Books</h1>
+                </div>
+            </div>}
             <main className="mainBox">
                 {children}
             </main>
--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Book = ({ movie }) => {
 
+    console.log(movie)
     return (
         <div className="itemBox"> 
-            <h1>{movie.attributes.title}</h1>
+            <div>
+                <h1 className="itemTitle">{movie.attributes.title}</h1>
+                <p className="itemInfo"><strong>Release date:</strong> {movie.attributes.releaseDate}</p>
+                <p className="itemInfo"><strong>Length:</strong> {movie.attributes.length}</p>
+                <p className="itemInfo"><strong>Rating:</strong> {movie.attributes.rating}</p>
+            </div>
+            <ul className="tagList">
+                {movie.attributes.tags.data.map((tag) => {
+                    return <li className="tag" key={tag.id}>{tag.attributes.name}</li>
+                })}
+            </ul>
         </div>
     )
 }
